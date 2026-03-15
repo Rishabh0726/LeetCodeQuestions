8d04870a33b716a56cdc5875b2fa03a8d497bc52
Rishabh Tiwari <153733171+Rishabh0726@users.noreply.github.com>
2026-03-15 17:06:00 +0530

    Time: 1 ms (99.84%) | Memory: 43.6 MB (86.19%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        s=s.trim();
+        int i=0;
+        int sign=1;
+        long num=0;
+        if (i<s.length() && s.charAt(i)=='+'){
+            i++;
+        } else if (i<s.length() && s.charAt(i)=='-') {
+            i++;
+            sign=-1;
+        }
+        for ( ;i<s.length(); i++){
+            char c1=s.charAt(i);
+            if (c1>=48 && c1<=57){
+                num=(num*10)+(c1-48);
+
+                // clamp(matlab agar num int ki range se bahar ho raha ho to return kar int min/max)
+                if (num*sign>Integer.MAX_VALUE)
+                    return Integer.MAX_VALUE;
+                if (num*sign<Integer.MIN_VALUE)
+                    return Integer.MIN_VALUE;
+            }
+            else{
+                break;
+            }
+        }
+        return (int)num*sign;
+    }
+}
