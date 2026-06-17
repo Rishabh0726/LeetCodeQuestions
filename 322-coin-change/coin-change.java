edd4ea0114c070a4ca6472def2f1d81e1df67302
Rishabh Tiwari <153733171+Rishabh0726@users.noreply.github.com>
2026-06-17 13:35:22 +0530

    Time: 17 ms (47.21%) | Memory: 46.3 MB (70.74%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+
+        if(amount<1)
+        return 0;
+
+        Arrays.sort(coins);
+
+        int[] minCoinsDp=new int[amount+1];
+
+        for(int i=1; i<amount+1; i++){
+            minCoinsDp[i]=Integer.MAX_VALUE;
+            for(int coin : coins){
+                if(coin<=i && minCoinsDp[i-coin]!=Integer.MAX_VALUE){
+                    minCoinsDp[i]=Math.min(minCoinsDp[i], 
+                    1+minCoinsDp[i-coin]);
+                }
+            }
+        }
+        if(minCoinsDp[amount]==Integer.MAX_VALUE){
+           return -1;
+        }
+        return minCoinsDp[amount];
+    }
+}
